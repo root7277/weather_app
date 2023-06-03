67b742a2df17005ad4ec23176cad38b3f4c088eb
Sanjar17 <sanjarsaidov173@gmail.com>
2023-06-03 09:28:11 +0500

    initial

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const WeatherScreen());
+}
+
+
+class WeatherScreen extends StatelessWidget {
+  const WeatherScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
